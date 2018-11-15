0c64ed920dde02b1b3f68e336c30c716c4f6cbb3
Catherine Bergevin <Bobby@imac-de-lyne.local>
2018-11-15 13:55:44 -0500

    Initial Commit

---
+++ Section2Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Section2Project
+//
+//  Created by Catherine Bergevin on 2018-11-15.
+//  Copyright © 2018 BobbySoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
